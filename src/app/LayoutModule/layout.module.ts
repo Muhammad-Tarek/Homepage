7b772af64fb4d7c0d19978b14070cd0e4ffc5c5b
Muhammad-Tarek <muhammadtarek221@gmail.com>
2023-10-12 02:25:16 +0200

    install font awesome + set app font +  create layout module + craete style helpers files

---
+++ src/app/LayoutModule/layout.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LayoutComponent } from './Pages/layout/layout.component';
+import { LayoutRoutingModule } from './layout-routing.module';
+
+@NgModule({
+  declarations: [HeaderComponent, FooterComponent, LayoutComponent],
+  imports: [CommonModule, LayoutRoutingModule],
+})
+export class LayoutModule {}
