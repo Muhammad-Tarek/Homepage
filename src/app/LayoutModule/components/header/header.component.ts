2f4347f4155c0382b5c3e5ecf71a128b6535a82a
Muhammad-Tarek <muhammadtarek221@gmail.com>
2023-10-15 21:16:50 +0200

    enhancements

--- src/app/LayoutModule/components/header/header.component.ts
+++ src/app/LayoutModule/components/header/header.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  isShownMenu!: boolean;
 
+  constructor() {}
+
+  toggleMenu() {
+    this.isShownMenu = !this.isShownMenu;
+  }
 }
